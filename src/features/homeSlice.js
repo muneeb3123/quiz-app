cbc59f4ba94e22c9201fb5bdbf017cac55a25cfc
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-11-26 21:59:37 +0500

    create home slice

---
+++ src/features/homeSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const subjectSlice = createSlice({
+name: "subject",
+initialState: {
+    subject: []
+},
+reducers: {}
+})
+
+export default subjectSlice.reducer;
