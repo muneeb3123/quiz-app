ec3c6c75e38de26f817ddaf57581c1bb97362331
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-11-29 19:41:23 +0500

    create Quiz slice and reducers

---
+++ src/features/quizSlice.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const quizSlice = createSlice({
+  initialState: {
+    quiz = [],
+  },
+  name: "quiz",
+  reducers: {
+    addQuiz: (state, action) => {
+      state.quiz = action.payload;
+    },
+    deleteQuiz: (state, action) => {
+      state.quiz = action.payload;
+    },
+    updateQuiz: (state, action) => {
+      state.quiz = action.payload;
+    },
+    getQuiz: (state, action) => {
+      state.quiz = action.payload;
+    },
+  },
+});
+
+export quizSlice.reducer;
