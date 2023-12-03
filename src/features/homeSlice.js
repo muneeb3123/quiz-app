f89fad53b68e52c0f5cd2a45d6f851fa0dc771c2
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-12-03 20:03:52 +0500

    add Loading

--- src/features/homeSlice.js
+++ src/features/homeSlice.js
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit"
 const homeSlice = createSlice({
 name: "subject",
 initialState: {
-    subject: []
+    subject: [],
+    isLoading: false,
 },
 reducers: {
     addSubject: (state, action) => {
         state.subject = action.payload;
+        state.isLoading = true;
     },
     
     deleteSubject: (state, action) => {
