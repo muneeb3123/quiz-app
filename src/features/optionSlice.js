2b174470b7ac8d36f4c59f24edf67468a64f2e28
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-12-19 21:42:15 +0500

    update slice

--- src/features/optionSlice.js
+++ src/features/optionSlice.js
@@ -9,6 +9,8 @@ initialState: {
 reducers: {
     addOption: (state, action) => {
         state.option = action.payload;
+        state.isLoading = true;
+
     },
     
     deleteOption: (state, action) => {
