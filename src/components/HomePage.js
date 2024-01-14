efcd6559cebab9b574e49746b60702eb48dde821
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-01-14 22:10:44 +0500

    update home component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,6 +4,9 @@ const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>lorem20
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
     </div>
   )
 }
