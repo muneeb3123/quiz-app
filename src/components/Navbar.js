a64551a9ac62ad864303fa68078bd65a34652521
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-01-28 20:57:57 +0500

    add links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-      
+      <a href='home'>Home</a>
+      <a href='Talha'>Talha</a>
+      <a href='muneeb'>Muneeb</a>
     </div>
   )
 }
