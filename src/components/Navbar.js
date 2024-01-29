f5c28dd59071e0463240490dc4d9eb272f7f2047
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-01-29 20:36:41 +0500

    add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
+      <h1>Logo</h1>
       <a href='home'>Home</a>
       <a href='Talha'>Talha</a>
       <a href='muneeb'>Muneeb</a>
