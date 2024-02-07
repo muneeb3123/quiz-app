51af47bd2eaf59ea42fe1a1b7d31f67d76649d78
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-02-07 21:38:16 +0500

    add heading 4

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@ function Contact() {
   return (
     <div>
     <h1>Contact</h1>
+    <h4>Get in touch</h4>
     <p>Feel free to contact me at</p>
     <p>email: <a href="mailto:">a</a></p>
     </div>
