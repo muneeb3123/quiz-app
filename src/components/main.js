627abc849ca3fd8a33b0c63b4666c345f1743525
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-02-10 20:15:29 +0500

    update

--- src/components/main.js
+++ src/components/main.js
@@ -5,6 +5,7 @@ const main = () => {
     <div>
       <h1>Heading</h1>
       <p>Paragraph</p>
+      <h3>Heading</h3>
       <p>Another paragraph</p>
       <img alt="kkk" src='uiiii'></img>
     </div>
