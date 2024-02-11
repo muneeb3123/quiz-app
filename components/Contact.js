a1913eba15320e8ee451b6a55cc5b365279319a7
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-02-11 21:12:50 +0500

    remove extra content

--- components/Contact.js
+++ components/Contact.js
@@ -5,9 +5,6 @@ function Contact() {
     <div>
     <h1>Contact</h1>
     <h4>Get in touch</h4>
-    <p>Feel free to contact me at</p>
-    <p>email: <a href="mailto:">a</a></p>
-    <p>phone: <a href="tel:">b</a></p>
     </div>
   )
 }
