22ecffd1b99cb4b5a25ca6f335c0961f79609692
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-02-21 00:14:54 +0500

    wrap links in div

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Logo</h1>
+      <div>
+      <a href='home'>Home</a>
+      <a href='Talha'>Talha</a>
+      <a href='muneeb'>Muneeb</a>
+      <a href='contact'>Contact</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
